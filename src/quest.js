6af756a8614338a141326529ffc8faa0891a9d85
Michelle Wang <tianw0420@gmail.com>
2022-02-07 15:52:29 -0800

    ability to add quests

--- src/quest.js
+++ src/quest.js
@@ -2,16 +2,39 @@ const quests = (() => {
     const addQuestBut = document.getElementById('add-quest-button'); 
     const navItems = document.getElementById('nav-items'); 
 
-    let _quests = {}; 
+    let _quests = []; 
     addQuestBut.addEventListener('click', () => {
         const input = document.createElement('input');
         input.type = 'text'; 
+        input.classList.add('input-textbox'); 
         navItems.appendChild(input); 
 
+        const submitBut = document.createElement('button'); 
+        submitBut.textContent = 'Add'; 
+        submitBut.addEventListener('click', () => {
+            const name = input.value;
+            input.remove(); 
+            submitBut.remove(); 
+            createQuest(name); 
+            addQuestToNav(name); 
+        })
+        
+        navItems.appendChild(submitBut); 
     }); 
-    function createQuest() {
-
+    function createQuest(name) {
+        const newQuest = {name: name}; 
+        _quests.push(newQuest);  
     }
+    function addQuestToNav(name){
+        const newQuest = document.createElement('li'); 
+        newQuest.classList.add('quest-item'); 
+        const newQuestLink = document.createElement('a'); 
+        newQuestLink.classList.add('quest-item-a'); 
+        newQuestLink.textContent = name; 
+        newQuest.appendChild(newQuestLink); 
+        navItems.appendChild(newQuest); 
+    }
+
     //func to create commission 
         //creates commission object with commission name & commission subtasks 
         //func to create commission subtask 
